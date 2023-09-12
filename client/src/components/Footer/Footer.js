d93d166519ad605b8641998dd1039e7b3df60c52
Jmour Dhia Eddine <125797700+Jmour-dh@users.noreply.github.com>
2023-09-12 15:29:19 +0200

    header footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.scss";
+import {
+  AiFillFacebook,
+  AiFillInstagram,
+  AiFillYoutube,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 
 function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
-    <div>
-      footer
-    </div>
-  )
+    <footer className={styles.footer}>
+      <ul className={styles.footerIcons}>
+        <li className={styles.socialIcons}>
+          <AiFillFacebook />
+        </li>
+        <li className={styles.socialIcons}>
+          <AiFillInstagram />
+        </li>
+        <li className={styles.socialIcons}>
+          <AiFillYoutube />
+        </li>
+        <li className={styles.socialIcons}>
+          <AiFillTwitterCircle />
+        </li>
+      </ul>
+      <div>
+        <p>Copyright © {year} Royal Place HOTELS & RESORTS </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.scss";
+import { NavLink } from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
 
 function Header() {
   return (
-    <div>
-      header
-    </div>
-  )
+    <header
+      className={`${styles.header} d-flex  flex-column justify-content-center align-items-center`}
+    >
+      <div
+        className={`${styles.logo} d-flex flex-column justify-content-center align-items-center`}
+      >
+        <span>Royal Palace</span>
+        <p>HOTELS & RESORTS</p>
+      </div>
+      <ul className="d-flex p-5">
+        <li className="mr-10">Accueil</li>
+        <li className="mr-10 ml-10">Restaurants & Bars</li>
+        <li className="mr-10 ml-10">Gallery</li>
+        <li className="mr-10 ml-10">Location & Details</li>
+        <li className="mr-10 ml-10">Reserver</li>
+        <li className=" ml-10">
+          <AiOutlineUser style={{ marginBottom: "2px" }} />
+        </li>
+      </ul>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
