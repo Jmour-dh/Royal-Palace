d8dea7487a8df17c999627f3eaa8d99fd6fb88eb
Riyad <riyad2593@gmail.com>
2023-09-21 12:52:17 +0200

    test

--- client/src/components/pages/gallery/components/tabsComponents/Hotel_Gallery.js
+++ client/src/components/pages/gallery/components/tabsComponents/Hotel_Gallery.js
@@ -5,7 +5,11 @@ import hotel2 from "../../../../../assets/images/tabs_hotel2.webp"
 import hotel3 from "../../../../../assets/images/tabs_hotel3.jpg"
 import hotel4 from "../../../../../assets/images/tabs_hotel4.jpg"
 
+
 function Hotel_Gallery() {
+    const images =[
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Ffr%2Fimages%2Fanimals%2Fwolf&psig=AOvVaw2H0oL1feVnQ8Nm6DVb_PiP&ust=1695214261271000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjz0rDbtoEDFQAAAAAdAAAAABAE"
+    ]
     return (
         <div className={styles.Tabs_Gallery}>
             <div className={styles.descriptiontabs}>
--- client/src/components/pages/profile/Profile.js
+++ client/src/components/pages/profile/Profile.js
@@ -1,16 +1,26 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../components/context";
+import styles from "./Profile.module.scss"
+import avatarImg from "../../../assets/images/utilisateur.png"
 
 function Profile() {
   const { user } = useContext(AuthContext);
   return (
-    <div>
-      <ul>
-        <li>{user.name}</li>
-        <li>{user.lname}</li>
-        <li>{user.email}</li>
+    <div className={styles.Profile}>
+      <h1>Profile</h1>
+      <div className={styles.ProfileContainer}>
+        <div className={styles.Pdp}>
+          <img src={avatarImg} />
+        </div>
+        <div className={styles.descriptionProfile}>
+          <ul>
+            <li className={styles.liPrecision}>Nom :<li className={styles.liuser}>{user.name}</li></li>
+            <li className={styles.liPrecision}>Prenom :<li className={styles.liuser}>{user.lname}</li></li>
+            <li className={styles.liPrecision}>Adresse mail :<li className={styles.liuser}>{user.email}</li></li>
       </ul>
+        </div>
     </div>
+  </div >
   );
 }
 
