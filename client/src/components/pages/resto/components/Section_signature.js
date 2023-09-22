af6f793aeb7679f1e72f839de98eb1cdabb881b4
Saphyr25 <riyad2593@gmail.com>
2023-09-22 15:43:53 +0200

    responsive page resto

--- client/src/components/pages/resto/components/Section_signature.js
+++ client/src/components/pages/resto/components/Section_signature.js
@@ -11,16 +11,20 @@ function Section_signature() {
                 <p>Des plats Suisses comme vous n'en avez jamais goutés auparavant, débordants d'authenticité, avec un tourbillon et une grande touche de créativité culinaire de Royal Palace.</p>
             </div>
             <div className={styles.Grid_section_signature}>
-                <img src={S1Img} alt="" />
-                <img src={S2Img} alt="" />
-                <div className={styles.GridP}>
-                    <h1>Zürich-Style Veal</h1>
-                    <p>Un succulent veau dans une appétissante sauce à la crème et aux champignons, servi avec un rösti croustillant traditionnel, un plat suisse par excellence populaire dans les restaurants Mövenpick</p>
+                <div className={styles.GridContaint}>
+                    <img src={S1Img} alt="" />
+                    <div className={styles.GridP}>
+                        <h1>Riz Casimir</h1>
+                        <p>Les saveurs de curry et de fruits tropicaux suggèrent une origine exotique, pourtant le Riz Casimir a été inventé dans la cuisine
+                            Mövenpick par notre fondateur, le légendaire Ueli Prager. </p>
+                    </div>
                 </div>
-                <div className={styles.GridP}>
-                    <h1>Riz Casimir</h1>
-                    <p>Les saveurs de curry et de fruits tropicaux suggèrent une origine exotique, pourtant le Riz Casimir a été inventé dans la cuisine
-                        Mövenpick par notre fondateur, le légendaire Ueli Prager. </p>
+                <div className={styles.GridContaint}>
+                    <img src={S2Img} alt="" />
+                    <div className={styles.GridP}>
+                        <h1>Zürich-Style Veal</h1>
+                        <p>Un succulent veau dans une appétissante sauce à la crème et aux champignons, servi avec un rösti croustillant traditionnel, un plat suisse par excellence populaire dans les restaurants Mövenpick</p>
+                    </div>
                 </div>
             </div>
         </div>
